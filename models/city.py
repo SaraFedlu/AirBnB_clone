445e1338ed7679e8e5dfed7b15017d97cacc67db
Sara <105264543+SaraFedlu@users.noreply.github.com>
2023-08-22 12:19:50 +0300

    another update

--- models/city.py
+++ models/city.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""Defines the City class."""
+""" Class City """
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """Represent a city.
-
-    Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city.
-    """
+    """Class City"""
 
     state_id = ""
     name = ""
